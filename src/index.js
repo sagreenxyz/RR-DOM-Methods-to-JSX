daccc6be228ef57581a20081888646e77e73bf48
Scott Green <sagreenxyz@gmail.com>
2022-03-21 13:08:20 -0500

    11. condensing forEach() to .map(); does not yet work

--- src/index.js
+++ src/index.js
@@ -1,39 +1,20 @@
-let koalaContainer = <div className="ui items"></div>
+let koalaContainer = (
 
-// Koala Card
-// koalas are in './public/koalas.js'
-koalas.forEach(koala => {
-    let koalaCard = <div className="item" style={{cursor: 'pointer'}}></div>
-
-    // Koala Card Image
-    let imageContainer = <div className="image"></div>
-    let koalaImage = <img src={koala.imageURL}></img>
-
-    imageContainer.append(koalaImage)
-
-    // Koala Card Content
-    let koalaContent = <div className="content"></div>
-
-    let header = <div className="header"></div>
-    header.append(koala.name)
-
-    let descriptionParagraph = <p></p>
-    descriptionParagraph.append(koala.description)
-
-    let descriptionContainer = <div className="description"></div>
-    descriptionContainer.append(descriptionParagraph)
-
-    koalaContent.append(
-        header,
-        descriptionContainer
-    )
-
-    koalaCard.append(
-        imageContainer,
-        koalaContent
-    )
-
-    koalaContainer.append(koalaCard)
-})
-
-document.body.append(koalaContainer)
+    <div className="ui items">
+        {koalas.map(koala => {
+            <div className="item" style={{ cursor: 'pointer' }}>
+                <div className="image">
+                    <img src={koala.imageURL}></img>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {koala.name}
+                    </div>
+                    <div className="description">
+                        <p>{koala.description}</p>
+                    </div>
+                </div>
+            </div>
+        })}
+    </div>
+)
